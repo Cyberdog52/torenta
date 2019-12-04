386368e95002ff5a857ef5a6e4885a687c3b91ba
anko <andres.konrad@hotmail.com>
2019-12-04 20:25:58 +0100

    nicer series overview

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 # torenta
 
-Useful links that helped the implementation:
-https://pub.tik.ee.ethz.ch/students/2006-So/MA-2006-26.pdf
-http://www.kristenwidman.com/blog/71/how-to-write-a-bittorrent-client-part-2/
-https://wiki.theory.org/index.php/BitTorrentSpecification
-
-
 Run Backend:
 Execute TorentaApplication through RunConfigurations
 Will start at http://localhost:8080/
@@ -15,7 +9,18 @@ cd frontend
 npm start
 Will start at http://localhost:4200/
 
-Access Swagger:
-http://localhost:8080/swagger-ui.html
-Docs: http://localhost:8080/v2/api-docs
+Swagger UI: http://localhost:8080/swagger-ui.html
+
+Swagger Docs: http://localhost:8080/v2/api-docs
+
+TMDB:
+https://developers.themoviedb.org/3/getting-started/introduction
  
+
+Useful links that helped the implementation:
+https://pub.tik.ee.ethz.ch/students/2006-So/MA-2006-26.pdf
+http://www.kristenwidman.com/blog/71/how-to-write-a-bittorrent-client-part-2/
+https://wiki.theory.org/index.php/BitTorrentSpecification
+
+
+
--- frontend/src/app/series-detail/series-detail.component.ts
+++ frontend/src/app/series-detail/series-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {SearchService} from "../search/search.service";
 import {SeriesDetail} from "../shared/dto/SeriesDetail";
+import {Season} from "../shared/dto/Season";
 
 @Component({
   selector: 'series-detail',
@@ -12,6 +13,7 @@ export class SeriesDetailComponent implements OnInit {
   @Input() id : number;
   public seriesDetail: SeriesDetail;
 
+
   constructor(private searchService: SearchService) { }
 
   ngOnInit() {
@@ -22,6 +24,28 @@ export class SeriesDetailComponent implements OnInit {
 
   public isLoading(): boolean {
     return this.seriesDetail == null;
+  }
+
+
+  getBackgroundImageFor(seriesDetail: SeriesDetail): string {
+    if (seriesDetail.backdrop_path == null) {
+      return "../../assets/tvnotfound.png";
+    }
+    return "https://image.tmdb.org/t/p/original/" + seriesDetail.backdrop_path;
+  }
+
+  getRuntime(seriesDetail: SeriesDetail): string {
+    if (this.seriesDetail.episode_run_time.length > 0) {
+      return this.seriesDetail.episode_run_time[0].toString();
+    }
+    return "?"
+  }
+
+  getSeasonTitle(season: Season): string {
+    if (season.season_number < 10) {
+      return "S0" + season.season_number.toString();
+    }
+    return "S" + season.season_number.toString();
   }
 
 }
