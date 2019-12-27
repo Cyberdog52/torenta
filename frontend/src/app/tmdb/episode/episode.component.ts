e7e9dc170bce61b14715334d38d5cce2ea851e2d
anko <andres.konrad@hotmail.com>
2019-12-27 14:30:06 +0100

    [TOR-18] show already downloaded episodes to user

--- frontend/src/app/tmdb/episode/episode.component.ts
+++ frontend/src/app/tmdb/episode/episode.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Episode} from "../../shared/dto/tmdb/Episode";
 import {SeriesDetail} from "../../shared/dto/tmdb/SeriesDetail";
+import {DirectoryService} from "../../directory/directory.service";
+import {FileHierarchyDto} from "../../shared/dto/directory/FileHierarchyDto";
+import {DirectoryDto} from "../../shared/dto/directory/DirectoryDto";
 
 @Component({
   selector: 'episode',
@@ -11,9 +14,13 @@ export class EpisodeComponent implements OnInit {
 
   @Input() episode : Episode;
   @Input() seriesDetail: SeriesDetail;
+  @Input() alreadyDownloaded: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
+
 }
+
+
