02792f2eef67fe4a2afcfa2db47e0721fc85f006
anko <andres.konrad@hotmail.com>
2020-01-28 13:03:58 +0100

    [TOR-28] frontend refactoring. downloadStatus is not correct from backend yet

--- frontend/src/app/tmdb/episode/episode.component.ts
+++ frontend/src/app/tmdb/episode/episode.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TmdbEpisodeDto} from "../../shared/dto/tmdb/TmdbEpisodeDto";
 import {TmdbSeriesDetailDto} from "../../shared/dto/tmdb/TmdbSeriesDetailDto";
+import {DownloadStatus} from "../../shared/dto/library/DownloadStatus";
 
 @Component({
   selector: 'episode',
@@ -11,13 +12,24 @@ export class EpisodeComponent implements OnInit {
 
   @Input() tmdbEpisodeDto : TmdbEpisodeDto;
   @Input() seriesDetail: TmdbSeriesDetailDto;
-  @Input() alreadyDownloaded: boolean;
+  @Input() downloadStatus: DownloadStatus;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getDownloadText(): string {
+    switch (this.downloadStatus) {
+      case DownloadStatus.DOWNLOADING: return "Downloading";
+      case DownloadStatus.DOWNLOADED: return "Downloaded";
+      default: return "";
+    }
+  }
+
+  hasDownloadStatus() {
+    return this.downloadStatus!=null;
+  }
 }
 
 
