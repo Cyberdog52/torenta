143853a53d9628f1c56d9c16fde9fb94126cc902
anko <andres.konrad@hotmail.com>
2026-08-31 10:12:35 +0200

    upgrade backend - fix test by adding test-key

--- src/test/java/ch/andreskonrad/torenta/TorentaApplicationTests.java
+++ src/test/java/ch/andreskonrad/torenta/TorentaApplicationTests.java
@@ -3,7 +3,7 @@ package ch.andreskonrad.torenta;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "ch.andreskonrad.torenta.tmdb.service.key=test-key")
 class TorentaApplicationTests {
 
     @Test
